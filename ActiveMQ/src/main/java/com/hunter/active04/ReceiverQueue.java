70cc9aaedb957a951ad6faca6a2fafe50fbe6426
Hunter <hunter.chen@richdataco.com>
2021-09-29 00:49:32 +0800

    MapMessage, BytesMessage, Dead message queue config and use

--- ActiveMQ/src/main/java/com/hunter/active04/ReceiverQueue.java
+++ ActiveMQ/src/main/java/com/hunter/active04/ReceiverQueue.java
@@ -3,8 +3,10 @@ package com.hunter.active04;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.io.FileOutputStream;
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Map;
 
 public class ReceiverQueue {
     public static void main(String[] args) throws JMSException {
@@ -15,8 +17,7 @@ public class ReceiverQueue {
                 "admin",
                 "tcp://localhost:61616"
         );
-        // activeMQConnectionFactory.setTrustAllPackages(true);
-        System.out.println(Person.class.getPackage().getName());
+        activeMQConnectionFactory.setTrustAllPackages(true);
         // activeMQConnectionFactory.setTrustedPackages(Arrays.asList(Person.class.getPackage().getName()));
         // 2. 获取一个向ActiveMQ的连接
         Connection connection = activeMQConnectionFactory.createConnection();
@@ -26,6 +27,49 @@ public class ReceiverQueue {
         // 4. 找目的地，获取destination， 消费端，也会从这个目的地取消息
 
         Destination queue = session.createQueue("user");
+        Destination queue2 = session.createQueue("xxooQ.user");
+        MessageConsumer consumer2 = session.createConsumer(queue2);
+        consumer2.setMessageListener((message) -> {
+            if (message instanceof ObjectMessage) {
+                ObjectMessage objectMessage = (ObjectMessage) message;
+                try {
+                    Serializable object = objectMessage.getObject();
+                    Person person = (Person) object;
+                    System.out.println("ObjectMessage: " + person.toString());
+                    objectMessage.acknowledge();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                if (message instanceof MapMessage) {
+                    MapMessage mapMessage = (MapMessage) message;
+                    try {
+                        System.out.println("MapMessage: " + mapMessage.getString("name"));
+                        mapMessage.acknowledge();
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (message instanceof BytesMessage) {
+                    BytesMessage bytesMessage = (BytesMessage) message;
+                    try {
+                            /*System.out.println("BytesMessage: " + bytesMessage.readBoolean());
+                            System.out.println("BytesMessage: " + bytesMessage.readUTF());*/
+
+                        FileOutputStream fileOutputStream = new FileOutputStream("D://activemq.txt");
+                        byte[] bytes = new byte[1024];
+                        int len = 0;
+                        while ((len = bytesMessage.readBytes(bytes)) != -1) {
+                            fileOutputStream.write(bytes, 0 , len);
+                        }
+                        bytesMessage.acknowledge();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
 
         // 5. 消息消费者
         MessageConsumer consumer = session.createConsumer(queue);
@@ -36,11 +80,39 @@ public class ReceiverQueue {
                     try {
                         Serializable object = objectMessage.getObject();
                         Person person = (Person) object;
-                        System.out.println(person.toString());
-                        message.acknowledge();
+                        System.out.println("ObjectMessage: " + person.toString());
+                        objectMessage.acknowledge();
                     } catch (JMSException e) {
                         e.printStackTrace();
                     }
+                } else {
+                    if (message instanceof MapMessage) {
+                        MapMessage mapMessage = (MapMessage) message;
+                        try {
+                            System.out.println("MapMessage: " + mapMessage.getString("name"));
+                            mapMessage.acknowledge();
+                        } catch (JMSException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (message instanceof BytesMessage) {
+                        BytesMessage bytesMessage = (BytesMessage) message;
+                        try {
+                            /*System.out.println("BytesMessage: " + bytesMessage.readBoolean());
+                            System.out.println("BytesMessage: " + bytesMessage.readUTF());*/
+
+                            FileOutputStream fileOutputStream = new FileOutputStream("D://activemq.txt");
+                            byte[] bytes = new byte[1024];
+                            int len = 0;
+                            while ((len = bytesMessage.readBytes(bytes)) != -1) {
+                                fileOutputStream.write(bytes, 0 , len);
+                            }
+                            bytesMessage.acknowledge();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
                 }
             }
         });
--- ActiveMQ/src/main/java/com/hunter/active04/SenderQueue.java
+++ ActiveMQ/src/main/java/com/hunter/active04/SenderQueue.java
@@ -8,25 +8,6 @@ public class SenderQueue {
     public static void main(String[] args) throws JMSException, InterruptedException {
 
         // 1. 获取连接工厂
-        /*ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(
-                ActiveMQConnectionFactory.DEFAULT_USER,
-                ActiveMQConnectionFactory.DEFAULT_PASSWORD,
-                "tcp://localhost:61616"
-        );*/
-        /**
-         *  ActiveMQConnectionFactory.DEFAULT_USER 默认值是Null
-         *  在activemq.xml配置文件里配置插件，设置用户名密码。
-         * 		<plugins>
-         * 			<simpleAuthenticationPlugin>
-         * 				<users>
-         * 					<authenticationUser username="admin" password="admin" groups="admins,publishers,consumers"/>
-         * 					<authenticationUser username="publisher" password="publisher" groups="publishers,consumers"/>
-         * 					<authenticationUser username="consumer" password="consumer" groups="consumers"/>
-         * 					<authenticationUser username="guest" password="guest" groups="guests"/>
-         * 				</users>
-         * 			</simpleAuthenticationPlugin>
-         * 		</plugins>
-         */
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(
                 "admin",
                 "admin",
@@ -42,9 +23,22 @@ public class SenderQueue {
 
         // 5.1 消息创建者
         MessageProducer producer = session.createProducer(queue);
+        producer.setTimeToLive(1000);
         Person person = new Person("hunter", 21, 1);
         ObjectMessage objectMessage = session.createObjectMessage(person);
         producer.send(objectMessage);
+
+        MapMessage mapMessage = session.createMapMessage();
+        mapMessage.setString("name", "Hunter");
+        producer.send(mapMessage);
+
+        BytesMessage bytesMessage = session.createBytesMessage();
+        bytesMessage.writeBoolean(true);
+        bytesMessage.writeUTF("hi~");
+        producer.send(bytesMessage);
+
+
+
 
         // 6. 关闭连接
 
