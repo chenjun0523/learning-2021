100a93071f613cda91439155a55650923cc426ab
Hunter <hunter.chen@richdataco.com>
2021-09-30 11:30:12 +0800

    windowSize

---
+++ ActiveMQ/src/main/java/com/hunter/active06/SenderQueue.java
@@ -0,0 +1,48 @@
+package com.hunter.active06;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class SenderQueue {
+    public static void main(String[] args) throws JMSException, InterruptedException {
+
+        // 1. 获取连接工厂
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(
+                "admin",
+                "admin",
+                "tcp://localhost:61616?jms.producerWindowSize=1048576"
+        );
+        // 2. 获取一个向ActiveMQ的连接
+        Connection connection = activeMQConnectionFactory.createConnection();
+        // 3. 获取session
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 4. 找目的地，获取destination， 消费端，也会从这个目的地取消息
+
+        Queue queue = session.createQueue("user?producer.windowSize=1048576");
+
+        // 5.1 消息创建者
+        MessageProducer producer = session.createProducer(queue);
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+        producer.setTimeToLive(1000);
+
+        for (int i = 0; i < 10000; i++) {
+            TextMessage textMessage = session.createTextMessage();
+            textMessage.setText("hi~" + i);
+
+            producer.send(textMessage);
+        }
+
+
+
+
+
+
+
+        // 6. 关闭连接
+
+        session.close();
+        System.out.println("System exit...");
+        System.out.println();
+    }
+}
