718bffc9526dbbf290819765ca53933c0d5a831d
Hunter <hunter.chen@richdataco.com>
2021-10-03 22:11:20 +0800

    active03-springboot integration

---
+++ ActiveMQ03/src/main/java/com/hunter/activemq03/ActiveMq03Application.java
@@ -0,0 +1,13 @@
+package com.hunter.activemq03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMq03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveMq03Application.class, args);
+    }
+
+}
---
+++ ActiveMQ03/src/test/java/com/hunter/activemq03/ActiveMq03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunter.activemq03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMq03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
